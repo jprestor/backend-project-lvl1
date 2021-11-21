8e8ab7cfb26b855801e50b5feccf18f0cd2ae50f
Dmitriy Miroshnichenko <d.mir.posts@gmail.com>
2021-11-22 00:18:57 +0300

    Add isPrime func in utils.js

--- src/utils.js
+++ src/utils.js
@@ -31,3 +31,17 @@ export const getArithmeticProgression = () => {
 
   return arr;
 };
+
+export const isPrime = (num) => {
+  if (num === 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
