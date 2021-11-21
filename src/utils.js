17085c603c76a169e3fab5563242c016b86d7983
Dmitriy Miroshnichenko <d.mir.posts@gmail.com>
2021-11-21 20:23:20 +0300

    Add getGCD func in utils.js

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,16 @@ export const getRandomOperator = () => {
 
   return operators[getRandomNumber(2)];
 };
+
+export const getGCD = (a, b) => {
+  let divisor = 1;
+
+  for (let i = Math.min(a, b); i > 1; i -= 1) {
+    if (a % i === 0 && b % i === 0) {
+      divisor = i;
+      break;
+    }
+  }
+
+  return divisor;
+};
