9201760edd6799b2082db5ff65cde1f61c9d43ce
Dmitriy Miroshnichenko <d.mir.posts@gmail.com>
2021-11-30 15:39:29 +0300

    Refactor brain-calc game code

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -10,16 +10,18 @@ const calculate = (a, b, operator) => {
       return a + b;
     case '-':
       return a - b;
+    case '*':
+      return a * b;
 
     default:
-      return a * b;
+      throw new Error(`operation ${a} ${operator} ${b} is not supported`);
   }
 };
 
 const getQuestionAndAnswer = () => {
   const randomA = getRandomNumber();
   const randomB = getRandomNumber();
-  const randomOperator = operators[getRandomNumber(0, 2)];
+  const randomOperator = operators[getRandomNumber(0, operators.length)];
   const question = `${randomA} ${randomOperator} ${randomB}`;
   const answer = calculate(randomA, randomB, randomOperator);
 
